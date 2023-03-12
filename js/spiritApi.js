aee7f96f5ec18b9b67d6c2da3e7bd7d125ca188e
Kelly <kellygonza12@gmail.com>
2023-03-12 12:21:22 -0700

    Add checkbox check event listnerA

--- js/spiritApi.js
+++ js/spiritApi.js
@@ -26,17 +26,19 @@ fetch(url)
 
 	// this will only fetch the photos when it is called by the event listener
 
-
-function getFetch(){
-	const date = document.querySelector('input').value
-	console.log(date);
-	const container = document.querySelector('.raw-image-container')
-	container.innerText = '';
-
-	fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=${date}&api_key=nzch7KX7METbfXDHs74CXlhb46UGmJoVe1gZTZRL`)
+	
+	function getFetch(){
+		const date = document.querySelector('input').value
+		console.log(date);
+		const container = document.querySelector('.raw-image-container')
+		container.innerText = '';
+		
+		fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?earth_date=${date}&api_key=nzch7KX7METbfXDHs74CXlhb46UGmJoVe1gZTZRL`)
 		.then(res=> res.json())
 		.then(data=> {
 			console.log(data)
+			
+			// this will show each images in the page
 			data.photos.forEach(photo => {
 				const img = document.createElement('img');
 				img.src = photo.img_src;
@@ -44,33 +46,20 @@ function getFetch(){
 				img.classList.add('.photo');
 				container.appendChild(img);
 			});
-
+			
 			// get all checkboxes and add event listners to filter images
-			const cameraCheckboxes = document.querySelectorAll('facetitem-checkbox')
-			cameraCheckboxes.forEach(checkbox => {
-				checkbox.addEventListener('change'. filterPhotos);
-			});
+			const cameraCheckboxes = document.querySelectorAll('.facetitem-checkbox')
+			let selectedCameras = []
+			cameraCheckboxes.forEach(function(checkbox) {
+				checkbox.addEventListener('change', function() {
+					selectedCameras = Array.from(cameraCheckboxes).filter(i => i.checked).map(i => i.value)
+				console.log(selectedCameras);
+				}); // add 'change' to anki
+		
+			})
+
 		})
 		.catch(err => {
 			console.log(`error ${err}`)			
 		});	
-}
-
-function filterPhotos() {
-	const cameraCheckboxes = document.querySelectorAll('.facetitem-checkbox');
-	const selectedCameras = [];
-	cameraCheckboxes.forEach(checkbox => {
-		if(checkbox.checked) {
-			selectedCameras.push(checkbox.value);
-		}
-	});
-
-	const photoElements = document.querySelectorAll('.raw-img-container img');
-	photoElements.forEach(photo => {
-		if(selectedCameras.length === 0 || selectedCameras.includes(photo.camera.name)) {
-			photo.classList.remove('hidden');
-		}else{
-			photo.classList.add('hidden');
-		}
-	})
 }
